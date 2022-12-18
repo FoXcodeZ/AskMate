56f46d0ffc6650b43bc48efe7d915d728b19edaf
FoXcodeZ <m.grabowy@int.pl>
2022-12-18 06:07:27 +0100

    added bcrypt library

--- util.py
+++ util.py
@@ -1,4 +1,5 @@
 import datetime
+import bcrypt
 
 
 def get_linux_timestamp():
@@ -7,3 +8,14 @@ def get_linux_timestamp():
 
 def get_linux_date():
     return datetime.datetime.now().strftime("%Y-%m-%d")
+
+
+def hash_password(plain_text_password):
+    # By using bcrypt, the salt is saved into the hash itself
+    hashed_bytes = bcrypt.hashpw(plain_text_password.encode('utf-8'), bcrypt.gensalt())
+    return hashed_bytes.decode('utf-8')
+
+
+def verify_password(plain_text_password, hashed_password):
+    hashed_bytes_password = hashed_password.encode('utf-8')
+    return bcrypt.checkpw(plain_text_password.encode('utf-8'), hashed_bytes_password)
